6a5d49fd464d48c3ab702a15e94508958632012e
Natalie Burgei <natalieburgei04@gmail.com>
2023-12-17 22:59:41 +0300

    completed asssignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name,age,sex,color,temparament,alive)
+VALUES
+('Mr. Chocolate',5,'M','White','Cheerful',True),
+('Rowdy'46,'M','White','Rowdy',True),
+('Tabitha',13,'F','yellow','Dull',True),
+('Sergeant Brown',70,'M','Brown','Moody',True),
+('Melissa',15,'F','Pink','Hungry',False),
+('Grinch',25,'M','Green','Grinchy',True),
+('Wendy',50,'F','Black','Relaxed',False),
+(NULL ,34,'F','Purple','Sleepy',True)
